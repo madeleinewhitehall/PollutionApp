0ec8e6b0231d9b9c4b602b83e0dd12479d0449b3
madeleinewhitehall <madeleinewhitehall@hotmail.co.uk>
2016-02-19 13:41:21 +0000

    Front end UI

---
+++ wear/src/main/java/com/example/maddiewhitehall/pollutionapp/SoundActivity.java
@@ -0,0 +1,40 @@
+package com.example.maddiewhitehall.pollutionapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class SoundActivity extends Activity {
+
+    private TextView mTextView;
+    private ImageButton button;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+
+                button = (ImageButton) stub.findViewById(R.id.backButton);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        intent = new Intent(SoundActivity.this, MainActivityWear.class);
+                        startActivity(intent);
+                        //finish();
+                    }
+                });
+            }
+        });
+    }
+}
